77da579008fca407f60ddce18f4cab10aa0afd21
hjulieub <126211061+hjulieub@users.noreply.github.com>
2023-03-08 13:44:42 -0500

    commithello

--- Gitpractise_progject/src/com/gitpractise/classtest2.java
+++ Gitpractise_progject/src/com/gitpractise/classtest2.java
@@ -4,7 +4,8 @@ public class classtest2 {
 
 	public static void main(String[] args) {
 		
-		System.out.println(greeting team);
+		System.out.println(greetings, team);
+		System.out.println(hello, tem);
 		// TODO Auto-generated method stub
 		
 	}
